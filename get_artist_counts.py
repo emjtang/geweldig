3a99cda410feb8dc9f85a571af63bd0737bb7ae0
srosston <sarah@rosston.com>
2017-05-14 16:59:19 -0700

    smaller dataset with top 10 artists

--- get_artist_counts.py
+++ get_artist_counts.py
@@ -1,10 +1,11 @@
-# usage: cat museum_data.csv |  python get_artist_counts.py
+# usage: cat museum_data.csv |  python get_artist_counts.py ( > museum_data_top10.csv)
 
 from collections import Counter
 import sys
 
-
+top_ten = ["George Hendrik Breitner", "Jan Luyken", "Reinier Vinkeles", "Marius Bauer", "Isaac Israels", "Bernard Picart", "Rembrandt Harmensz. van Rijn", "Johannes Tavenraat", "Willem Witsen", "Simon Fokke"]
 c = Counter()
+
 max_width = -1
 max_height = -1
 
@@ -13,17 +14,20 @@ for line in sys.stdin:
 		continue
 	#print line
 	artist = line.split(",")[2]
-	if artist == "principalOrFirstMaker":
+	if artist in ["principalOrFirstMaker", "anonymous", ""]:
 		continue
 	c[artist] += 1
 	width = int(line.split(",")[5]) 
 	height = int(line.split(",")[6]) 
-	if width > max_width:
-		max_width = width
-	if height > max_width:
-		max_height = height
+	if artist in top_ten:
+		if c[artist] < 100:
+			print line
+		c[artist] += 1
 
 
-print c.most_common(30)
-print max_width
-print max_height
+
+
+
+#print c.most_common(30)
+#print max_width
+#print max_height
