365def3f959c65ed06ea056c8488f66212a8d51f
srosston <sarah@rosston.com>
2017-05-14 16:51:40 -0700

    resize to 224x224

--- pad_img_borders.py
+++ pad_img_borders.py
@@ -12,19 +12,24 @@ new_image_dir = image_dir.replace("images", "resized_images")
 if not os.path.exists(new_image_dir):
     os.makedirs(new_image_dir)
 
-#new_size = (18809, 18809)
-new_size = (7378, 7378)
+new_size = (224, 224)
 
 img_files = os.listdir(image_dir)
-
+print "here"
 for img_file in img_files:
-	if ".png" not in img_file and ".jpg" not in img_file:
+	if ".png" not in img_file and ".jpg" not in img_file and ".jpeg" not in img_file :
 		continue
 	old_im = Image.open(image_dir + img_file)
 	old_size = old_im.size
 
-	new_im = Image.new("RGB", new_size)   ## luckily, this is already black!
-	new_im.paste(old_im, ((new_size[0]-old_size[0])/2,
-	                      (new_size[1]-old_size[1])/2))
-	new_im.save(new_image_dir + img_file)
+
+	longest_side = max(old_size[0], old_size[1])
+	print longest_side
+	new_im = Image.new("RGB", (longest_side, longest_side)) 
+	# pad with black
+	new_im.paste(old_im, ((longest_side-old_size[0])/2,
+	                      (longest_side-old_size[1])/2))
+	# resize to 224x224
+	newest_im = new_im.resize(new_size)
+	newest_im.save(new_image_dir + img_file)
 	
