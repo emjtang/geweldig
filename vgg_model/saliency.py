6d4a7ddaf310a66a13e66218d3e4e48ea808cc68
Emily Tang <emjtang@stanford.edu>
2017-06-02 13:25:20 -0700

    saliency try 1

---
+++ vgg_model/saliency.py
@@ -0,0 +1,361 @@
+"""
+Example TensorFlow script for finetuning a VGG model on your own data.
+Uses tf.contrib.data module which is in release candidate 1.2.0rc0
+Based on:
+    - PyTorch example from Justin Johnson:
+      https://gist.github.com/jcjohnson/6e41e8512c17eae5da50aebef3378a4c
+Required packages: tensorflow (v1.2)
+You can install the release candidate 1.2.0rc0 here:
+https://www.tensorflow.org/versions/r1.2/install/
+Download the weights trained on ImageNet for VGG:
+```
+wget http://download.tensorflow.org/models/vgg_16_2016_08_28.tar.gz
+tar -xvf vgg_16_2016_08_28.tar.gz
+rm vgg_16_2016_08_28.tar.gz
+```
+For this example we will use a tiny dataset of images from the COCO dataset.
+We have chosen eight types of animals (bear, bird, cat, dog, giraffe, horse,
+sheep, and zebra); for each of these categories we have selected 100 training
+images and 25 validation images from the COCO dataset. You can download and
+unpack the data (176 MB) by running:
+```
+wget cs231n.stanford.edu/coco-animals.zip
+unzip coco-animals.zip
+rm coco-animals.zip
+```
+The training data is stored on disk; each category has its own folder on disk
+and the images for that category are stored as .jpg files in the category folder.
+In other words, the directory structure looks something like this:
+coco-animals/
+  train/
+    bear/
+      COCO_train2014_000000005785.jpg
+      COCO_train2014_000000015870.jpg
+      [...]
+    bird/
+    cat/
+    dog/
+    giraffe/
+    horse/
+    sheep/
+    zebra/
+  val/
+    bear/
+    bird/
+    cat/
+    dog/
+    giraffe/
+    horse/
+    sheep/
+    zebra/
+"""
+
+import argparse
+import os
+
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import tensorflow.contrib.slim.nets
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--train_dir', default='../data/images_top10/train')
+parser.add_argument('--model_path', default='vgg_16.ckpt', type=str)
+parser.add_argument('--batch_size', default=32, type=int)
+parser.add_argument('--num_workers', default=4, type=int)
+parser.add_argument('--num_epochs1', default=1, type=int)
+parser.add_argument('--num_epochs2', default=1, type=int)
+parser.add_argument('--learning_rate1', default=1e-3, type=float)
+parser.add_argument('--learning_rate2', default=1e-5, type=float)
+parser.add_argument('--dropout_keep_prob', default=0.5, type=float)
+parser.add_argument('--weight_decay', default=5e-4, type=float)
+
+VGG_MEAN = [123.68, 116.78, 103.94]
+output_file = "vgg_fc7_10epoch_loss.csv"
+
+def compute_saliency_maps(X, y, model):
+    """
+    Compute a class saliency map using the model for images X and labels y.
+
+    Input:
+    - X: Input images, numpy array of shape (N, H, W, 3)
+    - y: Labels for X, numpy of shape (N,)
+    - model: A SqueezeNet model that will be used to compute the saliency map.
+
+    Returns:
+    - saliency: A numpy array of shape (N, H, W) giving the saliency maps for the
+    input images.
+    """
+    saliency = None
+    # Compute the score of the correct class for each example.
+    # This gives a Tensor with shape [N], the number of examples.
+    #
+    # Note: this is equivalent to scores[np.arange(N), y] we used in NumPy
+    # for computing vectorized losses.
+    correct_scores = tf.gather_nd(model.classifier,
+                                  tf.stack((tf.range(X.shape[0]), model.labels), axis=1))
+    ###############################################################################
+    # TODO: Implement this function. You should use the correct_scores to compute #
+    # the loss, and tf.gradients to compute the gradient of the loss with respect #
+    # to the input image stored in model.image.                                   #
+    # Use the global sess variable to finally run the computation.                #
+    # Note: model.image and model.labels are placeholders and must be fed values  #
+    # when you call sess.run().                                                   #
+    ###############################################################################
+    grad = tf.gradients(correct_scores, model.image)[0]
+    
+    grad_val = sess.run(grad, feed_dict={model.image: X, model.labels: y})
+
+    saliency = np.amax(np.abs(grad_val), axis = 3)
+    
+    ##############################################################################
+    #                             END OF YOUR CODE                               #
+    ##############################################################################
+    return saliency
+
+def show_saliency_maps(X, y, mask, model):
+    mask = np.asarray(mask)
+    Xm = X[mask]
+    ym = y[mask]
+
+    saliency = compute_saliency_maps(Xm, ym, model)
+
+    for i in range(mask.size):
+        plt.subplot(2, mask.size, i + 1)
+        plt.imshow(deprocess_image(Xm[i]))
+        plt.axis('off')
+        plt.title(class_names[ym[i]])
+        plt.subplot(2, mask.size, mask.size + i + 1)
+        plt.title(mask[i])
+        plt.imshow(saliency[i], cmap=plt.cm.hot)
+        plt.axis('off')
+        plt.gcf().set_size_inches(10, 4)
+    plt.show()
+
+
+def list_images(directory):
+    """
+    Get all the images and labels in directory/label/*.jpg
+    """
+    labels = os.listdir(directory)
+    files_and_labels = []
+    for label in labels:
+        for f in os.listdir(os.path.join(directory, label)):
+            files_and_labels.append((os.path.join(directory, label, f), label))
+
+    filenames, labels = zip(*files_and_labels)
+    filenames = list(filenames)
+    labels = list(labels)
+    unique_labels = list(set(labels))
+
+    label_to_int = {}
+    for i, label in enumerate(unique_labels):
+        label_to_int[label] = i
+
+    labels = [label_to_int[l] for l in labels]
+
+    return filenames, labels
+
+
+def check_loss(sess, loss, is_training, dataset_init_op):
+    """
+    Check the accuracy of the model on either train or val (depending on dataset_init_op).
+    """
+    # Initialize the correct dataset
+    sess.run(dataset_init_op)
+    total_loss = 0
+    while True:
+        try:
+            total_loss = sess.run(loss, {is_training: False})
+        except tf.errors.OutOfRangeError:
+            break
+
+    # Return the fraction of datapoints that were correctly classified
+    return total_loss
+
+
+
+def check_accuracy(sess, correct_prediction, is_training, dataset_init_op):
+    """
+    Check the accuracy of the model on either train or val (depending on dataset_init_op).
+    """
+    # Initialize the correct dataset
+    sess.run(dataset_init_op)
+    num_correct, num_samples = 0, 0
+    while True:
+        try:
+            correct_pred = sess.run(correct_prediction, {is_training: False})
+            num_correct += correct_pred.sum()
+            num_samples += correct_pred.shape[0]
+        except tf.errors.OutOfRangeError:
+            break
+
+    # Return the fraction of datapoints that were correctly classified
+    acc = float(num_correct) / num_samples
+    return acc
+
+
+def main(args):
+    # Get the list of filenames and corresponding list of labels for training et validation
+    train_filenames, train_labels = list_images(args.train_dir)
+
+    num_classes = len(set(train_labels))
+
+    # --------------------------------------------------------------------------
+    # In TensorFlow, you first want to define the computation graph with all the
+    # necessary operations: loss, training op, accuracy...
+    # Any tensor created in the `graph.as_default()` scope will be part of `graph`
+    graph = tf.Graph()
+    with graph.as_default():
+        # Standard preprocessing for VGG on ImageNet taken from here:
+        # https://github.com/tensorflow/models/blob/master/slim/preprocessing/vgg_preprocessing.py
+        # Also see the VGG paper for more details: https://arxiv.org/pdf/1409.1556.pdf
+
+        # Preprocessing (for both training and validation):
+        # (1) Decode the image from jpg format
+        # (2) Resize the image so its smaller side is 256 pixels long
+        def _parse_function(filename, label):
+            image_string = tf.read_file(filename)
+            image_decoded = tf.image.decode_jpeg(image_string, channels=3)          # (1)
+            image = tf.cast(image_decoded, tf.float32)
+
+            smallest_side = 225.0
+            height, width = tf.shape(image)[0], tf.shape(image)[1]
+            height = tf.to_float(height)
+            width = tf.to_float(width)
+
+            scale = tf.cond(tf.greater(height, width),
+                            lambda: smallest_side / width,
+                            lambda: smallest_side / height)
+            new_height = tf.to_int32(height * scale)
+            new_width = tf.to_int32(width * scale)
+
+            resized_image = tf.image.resize_images(image, [new_height, new_width])  # (2)
+            return resized_image, label
+
+        # Preprocessing (for training)
+        # (3) Take a random 224x224 crop to the scaled image
+        # (4) Horizontally flip the image with probability 1/2
+        # (5) Substract the per color mean `VGG_MEAN`
+        # Note: we don't normalize the data here, as VGG was trained without normalization
+        def training_preprocess(image, label):
+            crop_image = tf.random_crop(image, [224, 224, 3])                       # (3)
+
+            means = tf.reshape(tf.constant(VGG_MEAN), [1, 1, 3])
+            centered_image = crop_image - means                                     # (5)
+
+            return centered_image, label
+
+
+     	
+        # ----------------------------------------------------------------------
+        # DATASET CREATION using tf.contrib.data.Dataset
+        # https://github.com/tensorflow/tensorflow/tree/master/tensorflow/contrib/data
+
+        # The tf.contrib.data.Dataset framework uses queues in the background to feed in
+        # data to the model.
+        # We initialize the dataset with a list of filenames and labels, and then apply
+        # the preprocessing functions described above.
+        # Behind the scenes, queues will load the filenames, preprocess them with multiple
+        # threads and apply the preprocessing in parallel, and then batch the data
+
+        # Training dataset
+        train_filenames = tf.constant(train_filenames)
+        train_labels = tf.constant(train_labels)
+        train_dataset = tf.contrib.data.Dataset.from_tensor_slices((train_filenames, train_labels))
+        train_dataset = train_dataset.map(_parse_function,
+            num_threads=args.num_workers, output_buffer_size=args.batch_size)
+        train_dataset = train_dataset.map(training_preprocess,
+            num_threads=args.num_workers, output_buffer_size=args.batch_size)
+        train_dataset = train_dataset.shuffle(buffer_size=10000)  # don't forget to shuffle
+        batched_train_dataset = train_dataset.batch(args.batch_size)
+
+
+        # Now we define an iterator that can operator on either dataset.
+        # The iterator can be reinitialized by calling:
+        #     - sess.run(train_init_op) for 1 epoch on the training set
+        #     - sess.run(val_init_op)   for 1 epoch on the valiation set
+        # Once this is done, we don't need to feed any value for images and labels
+        # as they are automatically pulled out from the iterator queues.
+
+        # A reinitializable iterator is defined by its structure. We could use the
+        # `output_types` and `output_shapes` properties of either `train_dataset`
+        # or `validation_dataset` here, because they are compatible.
+        iterator = tf.contrib.data.Iterator.from_structure(batched_train_dataset.output_types,
+                                                           batched_train_dataset.output_shapes)
+        images, labels = iterator.get_next()
+
+        train_init_op = iterator.make_initializer(batched_train_dataset)
+
+        # Indicates whether we are in training or in test mode
+        is_training = tf.placeholder(tf.bool)
+
+        # ---------------------------------------------------------------------
+        # Now that we have set up the data, it's time to set up the model.
+        # For this example, we'll use VGG-16 pretrained on ImageNet. We will remove the
+        # last fully connected layer (fc8) and replace it with our own, with an
+        # output size num_classes=8
+        # We will first train the last layer for a few epochs.
+        # Then we will train the entire model on our dataset for a few epochs.
+
+        # Get the pretrained model, specifying the num_classes argument to create a new
+        # fully connected replacing the last one, called "vgg_16/fc8"
+        # Each model has a different architecture, so "vgg_16/fc8" will change in another model.
+        # Here, logits gives us directly the predicted scores we wanted from the images.
+        # We pass a scope to initialize "vgg_16/fc8" weights with he_initializer
+        vgg = tf.contrib.slim.nets.vgg
+        with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=args.weight_decay)):
+            logits, _ = vgg.vgg_16(images, num_classes=num_classes, is_training=is_training,
+                                   dropout_keep_prob=args.dropout_keep_prob)
+
+        # Specify where the model checkpoint is (pretrained weights).
+        model_path = args.model_path
+        #assert(os.path.isfile(model_path))
+
+        # Restore only the layers up to fc7 (included)
+        # Calling function `init_fn(sess)` will load all the pretrained weights.
+        variables_to_restore = tf.contrib.framework.get_variables_to_restore(exclude=[])
+        init_fn = tf.contrib.framework.assign_from_checkpoint_fn(model_path, variables_to_restore)
+
+        # ---------------------------------------------------------------------
+        # Using tf.losses, any loss is added to the tf.GraphKeys.LOSSES collection
+        # We can then call the total loss easily
+        tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+        loss = tf.losses.get_total_loss()
+
+        # First we want to train only the reinitialized last 3 layers fc6, fc7, fc8 for a few epochs.
+        # We run minimize the loss only with respect to the fc8 variables (weight and bias).i
+
+        # Then we want to finetune the entire model for a few epochs.
+        # We run minimize the loss only with respect to all the variables.
+
+        # Evaluation metrics
+        prediction = tf.to_int32(tf.argmax(logits, 1))
+        correct_prediction = tf.equal(prediction, labels)
+        accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+        tf.get_default_graph().finalize()
+
+    # --------------------------------------------------------------------------
+    # Now that we have built the graph and finalized it, we define the session.
+    # The session is the interface to *run* the computational graph.
+    # We can call our training operations with `sess.run(train_op)` for instance
+ 
+    with tf.Session(graph=graph) as sess:
+        init_fn(sess)  # load the pretrained weights
+        #mask = np.arange(5)
+        #show_saliency_maps(X, y, mask)
+        classifier = tf.reshape(graph,[-1, num_classes])
+       
+        # Train the entire model for a few more epochs, continuing with the *same* weights.
+        for epoch in range(args.num_epochs2):
+            print('Starting epoch %d / %d' % (epoch + 1, args.num_epochs1))
+            sess.run(train_init_op)
+
+            # Check accuracy on the train and val sets every epoch
+            train_acc = check_accuracy(sess, correct_prediction, is_training, train_init_op)
+            print('Train accuracy: %f' % train_acc)
+        #sess.run(tf.global_variables_initializer())
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
