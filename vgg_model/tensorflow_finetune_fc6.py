342c0bc2350fd108af7c46eb1cf46d2d972011af
Emily Tang <emjtang@stanford.edu>
2017-06-02 07:00:35 +0000

    lol batch_num fml

--- vgg_model/tensorflow_finetune_fc6.py
+++ vgg_model/tensorflow_finetune_fc6.py
@@ -64,8 +64,8 @@ parser.add_argument('--val_dir', default='../data/images_top10/val')
 parser.add_argument('--model_path', default='vgg_16.ckpt', type=str)
 parser.add_argument('--batch_size', default=32, type=int)
 parser.add_argument('--num_workers', default=4, type=int)
-parser.add_argument('--num_epochs1', default=5, type=int)
-parser.add_argument('--num_epochs2', default=1, type=int)
+parser.add_argument('--num_epochs1', default=15, type=int)
+parser.add_argument('--num_epochs2', default=5, type=int)
 parser.add_argument('--learning_rate1', default=1e-3, type=float)
 parser.add_argument('--learning_rate2', default=1e-5, type=float)
 parser.add_argument('--dropout_keep_prob', default=0.5, type=float)
@@ -377,7 +377,6 @@ def main(args):
             while True:
                 try:
                     _ = sess.run(full_train_op, {is_training: True})
-                    batch_num += 1
                 except tf.errors.OutOfRangeError:
                     break
 
