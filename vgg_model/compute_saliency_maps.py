ad955131d4237964ca95ec6df62d0b426030313e
Emily Tang <emjtang@stanford.edu>
2017-06-03 16:27:12 -0700

    few fixes in single test and compute saliency maps

--- vgg_model/compute_saliency_maps.py
+++ vgg_model/compute_saliency_maps.py
@@ -1,4 +1,6 @@
-from PIL import Image
+'''
+Compute saliency maps using PyTorch because Tensorflow SUCKS
+'''
 import argparse
 import torch
 import torch.nn as nn
@@ -17,6 +19,69 @@ parser.add_argument('--use_gpu', action='store_true')
 parser.add_argument('--batch_size', default=1, type=int)
 parser.add_argument('--num_workers', default=4, type=int)
 
+def compute_saliency_maps(X, y, model):
+    """
+    Compute a class saliency map using the model for images X and labels y.
+
+    Input:
+    - X: Input images; Tensor of shape (N, 3, H, W)
+    - y: Labels for X; LongTensor of shape (N,)
+    - model: A pretrained CNN that will be used to compute the saliency map.
+
+    Returns:
+    - saliency: A Tensor of shape (N, H, W) giving the saliency maps for the input
+    images.
+    """
+    # Make sure the model is in "test" mode
+    model.eval()
+    # dtype = torch.FloatTensor
+    
+    # Wrap the input tensors in Variables
+    X_var = Variable(X, requires_grad=True)
+    y_var = Variable(y)
+    saliency = None
+    ##############################################################################
+    # TODO: Implement this function. Perform a forward and backward pass through #
+    # the model to compute the gradient of the correct class score with respect  #
+    # to each input image. You first want to compute the loss over the correct   #
+    # scores, and then compute the gradients with a backward pass.               #
+    ##############################################################################
+    # model.type(dtype)
+
+    scores = model(X_var)
+
+    scores_arr = scores.gather(1, y_var.view(-1, 1)).squeeze()
+    sum_scores = torch.sum(scores_arr)
+
+    sum_scores.backward()
+    
+    saliency = X_var.grad.data.abs().max(dim=1)[0].squeeze()
+    
+    return saliency
+def show_saliency_maps(X, y, model):
+    # Convert X and y from numpy arrays to Torch Tensors
+    # X_tensor = torch.cat([preprocess(Image.fromarray(x)) for x in X])
+    X_tensor = torch.cat(X)
+    y_tensor = torch.LongTensor(y)
+
+    # Compute saliency maps for images in X
+    saliency = compute_saliency_maps(X_tensor, y_tensor, model)
+
+    # Convert the saliency map from Torch Tensor to numpy array and show images
+    # and saliency maps together.
+    saliency = saliency.numpy()
+    N = X.shape[0]
+    for i in range(N):
+        plt.subplot(2, N, i + 1)
+        plt.imshow(X[i])
+        plt.axis('off')
+        plt.title(class_names[y[i]])
+        plt.subplot(2, N, N + i + 1)
+        plt.imshow(saliency[i], cmap=plt.cm.hot)
+        plt.axis('off')
+        plt.gcf().set_size_inches(12, 5)
+    plt.show()
+
 def main(args):
   # Figure out the datatype we will use; this will determine whether we run on
   # CPU or on GPU. Run on GPU by adding the command-line flag --use_gpu
@@ -34,14 +99,12 @@ def main(args):
   # (4) Convert the image from a PIL Image to a Torch Tensor
   # (5) Normalize the image using the mean and variance of each color channel
   #     computed on the ImageNet dataset.
-
-  val_transform = T.Compose([
+  test_transform = T.Compose([
     T.Scale(224),
     T.CenterCrop(224),
     T.ToTensor(),
     T.Normalize(mean=IMAGENET_MEAN, std=IMAGENET_STD),
   ])
-
   # You load data in PyTorch by first constructing a Dataset object which
   # knows how to load individual data points (images and labels) and apply a
   # transform. The Dataset object is then wrapped in a DataLoader, which iterates
@@ -76,36 +139,33 @@ def main(args):
   #
   # You can read more about the ImageFolder class here:
   # https://github.com/pytorch/vision/blob/master/torchvision/datasets/folder.py
-  train_dset = ImageFolder(args.train_dir, transform=val_transform)
-  train_loader = DataLoader(train_dset,
-                    batch_size=args.batch_size,
-                    num_workers=args.num_workers,
-                    shuffle=True)
-
   model = torch.load("pytorch_full")
   model.eval()
-  #print(model)
-  filename=r'../data/images_top10/train/BernardPicart/en-NG-598-A.jpg'
-  for x, y in train_loader:
-    x_var = Variable(x.type(dtype))
-    y_var = Variable(y.type(dtype).long())
-    scores = model(x_var)
-    #print(scores)
-    _, preds = scores.data.cpu().max(1)
-    print("predicted:", preds[0][0], ", actual", y[0])
-  # model.eval()
-  img = Image.open(filename).convert('RGB')
-  inputVar = Variable(val_transform(img).unsqueeze(0))
-  prediction = model(inputVar)	 
-  print("prediction:", prediction)
 
-  probs, indices = (-nn.Softmax()(prediction).data).sort()
-  print("probs", probs)
-  print("indices", indices)
-  probs = (-probs).numpy()[0][:10]; indices = indices.numpy()[0][:10]
-  print(probs)
-  _, predicted = torch.max(prediction.data, 1)
-  print("predicted:", predicted[0][0], ", actual", y[0])
+  filename=r'../data/images_top10/train/BernardPicart/en-NG-598-A.jpg'
+
+
+
+  model = torch.load("pytorch_full")
+  #print(model)
+
+  img = Image.open(filename).convert('RGB')
+  X = test_transform(img).unsqueeze(0)
+  y = 0
+  show_saliency_maps([X], [y], model)
+  # for x, y in train_loader:
+  #   x_var = Variable(x.type(dtype))
+  #   y_var = Variable(y.type(dtype).long())
+  #   scores = model(x_var)
+  #   #print(scores)
+  #   _, preds = scores.data.cpu().max(1)
+  #   print(preds[0][0], y[0])
+   
+#  img = skimage.io.imread(filename)
+#  x = V(centre_crop(img).unsqueeze(0), volatile=True)
+#  logit = model(x)
+#  print(logit)
+  #model = pytorch_full
 
 if __name__ == '__main__':
   args = parser.parse_args()
