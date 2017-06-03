06e464ed5175cc6565544b3dff4668e58ee99df3
Emily Tang <emjtang@stanford.edu>
2017-06-03 02:25:21 +0000

    trying to test one img

--- vgg_model/test_img.py
+++ vgg_model/test_img.py
@@ -31,25 +31,29 @@ def importImg(filename):
   new_width = tf.to_int32(width * scale)
 
   resized_image = tf.image.resize_images(image, [new_height, new_width])  # (2)
-  return resized_image, label
+  return resized_image
 
 def main(args):
   graph = tf.Graph()
-  image, label = importImg("test.png")
-
-  vgg = tf.contrib.slim.nets.vgg
-  with slim.arg_scope(vgg.vgg_arg_scope(weight_decay=args.weight_decay)):
-      logits, _ = vgg.vgg_16(images, num_classes=num_classes, is_training=is_training,
-                             dropout_keep_prob=args.dropout_keep_prob)
+  image = importImg("test.jpg")
+  num_classes = 10
+  #vgg = tf.contrib.slim.nets.vgg
+  #with slim.arg_scope(vgg.vgg_arg_scope()):
+      #logits, _ = vgg.vgg_16(image, num_classes=num_classes, is_training=False,
+                             #dropout_keep_prob=.8)
 
   # Specify where the model checkpoint is (pretrained weights).
   model_path = args.model_path
-
-  variables_to_restore = tf.contrib.framework.get_variables_to_restore(exclude=[])
-  init_fn = tf.contrib.framework.assign_from_checkpoint_fn(model_path, variables_to_restore)
+  label = tf.placeholder(tf.int32)
+  #variables_to_restore = tf.contrib.framework.get_variables_to_restore(exclude=[])
+  #init_fn = tf.contrib.framework.assign_from_checkpoint_fn(model_path, variables_to_restore)
+  saver = tf.train.Saver()
+  ckpt = tf.train.get_checkpoint_state(args.ckpt_path)
   with graph.as_default():
     with tf.Session(graph=graph) as sess:
-      init_fn(sess)
+      saver.restore(sess, ckpt.model_checkpoint_path)
+      #softmaxval = sess.run(label, feed)
+      #init_fn(sess)
       # saver = tf.train.Saver()
       # ckpt = tf.train.get_checkpoint_state(args.ckpt_path)
       predictions = sess.run(label, feed_dict={x: image})
